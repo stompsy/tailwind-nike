62bf0c54ee4075a11b840db17610c8d6c55a4a0b
Brian Gerdes <brian.gerdes@protonmail.com>
2023-10-28 09:52:07 -0700

    services section added

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,13 +2,15 @@ const Button = ({ label, iconURL }) => {
 	return (
 		<button className='flex justify-center items-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none bg-coral-red rounded-full text-white border-coral-red'>
 			{label}
-			<img
-				src={iconURL}
-				alt='Arrow right icon'
-				className='ml-2 rounded-full'
-				width={5}
-				height={5}
-			/>
+			{iconURL && (
+				<img
+					src={iconURL}
+					alt='Arrow right icon'
+					className='ml-2 rounded-full'
+					width={5}
+					height={5}
+				/>
+			)}
 		</button>
 	);
 };
