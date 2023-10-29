82b264c86d26491b9e291162e0b4e581970f3369
Brian Gerdes <brian.gerdes@protonmail.com>
2023-10-29 11:27:49 -0700

    special offer section added

--- src/sections/SpecialOffer.jsx
+++ src/sections/SpecialOffer.jsx
@@ -1,5 +1,45 @@
+import { offer } from '../assets/images';
+import Button from '../components/Button';
+import { arrowRight } from '../assets/icons';
+
 const SpecialOffer = () => {
-	return <div>SpecialOffer</div>;
+	return (
+		<section className='flex justify-around items-center max-xl:flex-col-reverse gap-10 max-container'>
+			<div className='flex-1'>
+				<img
+					src={offer}
+					width={773}
+					height={687}
+					className='object-contain w-full'
+					alt='Special Offer'
+				/>
+			</div>
+			<div className='flex flex-1 flex-col'>
+				<h2 className='font-palanquin text-4xl capitalize font-bold lg:max-w-lg'>
+					<span className='text-coral-red'> Special</span> Shoes
+				</h2>
+				<p className='mt-4 lg:max-w-lg info-text'>
+					Embark on a shopping journey that redefines your experience with
+					unbeatable deals. From premier selections to incredible savings, we
+					offer unparalleled value that sets us apart.
+				</p>
+				<p className='mt-6 lg:max-w-lg info-text'>
+					Navigate a realm of possibilities designed to fulfill your unique
+					desires, surpassing the loftiest expectations. Your journey with us is
+					nothing short of exceptional.
+				</p>
+				<div className='flex mt-11 flex-wrap gap-4'>
+					<Button label='View Details' />
+					<Button
+						label='Learn More'
+						backgroundColor='bg-white'
+						borderColor='border-slate-gray'
+						textColor='text-slate-gray'
+					/>
+				</div>
+			</div>
+		</section>
+	);
 };
 
 export default SpecialOffer;
